1f989dda0725beea8027d0fb054373cac2ee692b
Igor Gunko <i.gunko.work@gmail.com>
2018-05-14 22:05:52 +0300

    Fixed to load Progress on scroll to block

--- dist/bundle.js
+++ dist/bundle.js
@@ -80,38 +80,47 @@ var ProgressBar = __webpack_require__(6);
 //import './js/slick.min.js';
 console.log('ok');
 
-$('.progress-circle').each(function (i) {
-    var circle = new ProgressBar.Circle(this, {
-        color: "red",
-        easing: 'linear',
-        strokeWidth: 5,
-        duration: 1500,
-        trailColor: '#b0cecc',
-        trailWidth: 5,
-        //fill: 'rgba(0, 0, 0, 0.1)',
-        text: {
-            value: '0'
-        }
-    });
-
-    circle.text.style.fontSize = '30px';
-    circle.text.style.color = '#fff';
-
-    var value = $(this).attr('value') / 100;
-
-    circle.animate(value, {
-        from: {
-            color: "#ccc"
-        },
-        to: {
-            color: "#60b8b7"
-        },
-        step: function step(state, circle, bar) {
-            circle.path.setAttribute('stroke', state.color);
-            circle.setText((circle.value() * 100).toFixed(0) + "%");
-        }
-    });
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 1050) {
+        loadProgress();
+        $(window).off("scroll");
+    }
 });
+
+function loadProgress() {
+    $('.progress-circle').each(function (i) {
+        var circle = new ProgressBar.Circle(this, {
+            color: "red",
+            easing: 'linear',
+            strokeWidth: 5,
+            duration: 3500,
+            trailColor: '#b0cecc',
+            trailWidth: 5,
+            //fill: 'rgba(0, 0, 0, 0.1)',
+            text: {
+                value: '0'
+            }
+        });
+
+        circle.text.style.fontSize = '30px';
+        circle.text.style.color = '#fff';
+
+        var value = $(this).attr('value') / 100;
+
+        circle.animate(value, {
+            from: {
+                color: "#ccc"
+            },
+            to: {
+                color: "#60b8b7"
+            },
+            step: function step(state, circle, bar) {
+                circle.path.setAttribute('stroke', state.color);
+                circle.setText((circle.value() * 100).toFixed(0) + "%");
+            }
+        });
+    });
+}
 
 /***/ }),
 /* 1 */
